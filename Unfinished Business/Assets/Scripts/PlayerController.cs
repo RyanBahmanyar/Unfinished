ad0928e91aa22437d4579cd640058743eabdbdd7
werignac <48932607+werignac@users.noreply.github.com>
2021-07-31 00:17:58 -0700

    Player Controller v0.9 complete
    This commit showcases an improved player controller with the ability to perform a light attack. This commit includes the scene and animations I've been using to test the player controller.

--- Unfinished Business/Assets/Scripts/PlayerController.cs
+++ Unfinished Business/Assets/Scripts/PlayerController.cs
@@ -2,14 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A controller that moves the player around.
+/// </summary>
 public class PlayerController : Moveable
 {
+    /// <summary>
+    /// The animator of the player.
+    /// </summary>
     private Animator anim;
 
+    /// <summary>
+    /// Whether the player can move (through inputs).
+    /// This is controlled with events in the player animations.
+    /// </summary>
     private bool canMove = true;
+    
+    /// <summary>
+    /// Whether the player can attack (through inputs).
+    /// This is controlled with events in the player animations.
+    /// </summary>
+    private bool canAttack = true;
 
+    /// <summary>
+    /// Whether the player is facing right or not.
+    /// </summary>
     [SerializeField]
     private bool facingRight;
+
+    /// <summary>
+    /// The name of the bool that the animator uses to play the walking animation.
+    /// </summary>
+    private const string walkingAnimatorKey = "Is Walking";
+
+    /// <summary>
+    /// The name of the trigger that the animator uses to play the light attack animation.
+    /// </summary>
+    private const string lightAttackAnimatorKey = "Light Attack";
 
     protected override void Awake()
     {
@@ -17,19 +46,63 @@ public class PlayerController : Moveable
         anim = GetComponent<Animator>();
     }
 
+    public void DisableMove()
+    {
+        canMove = false;
+    }
+
+    public void EnableMove()
+    {
+        canMove = true;
+    }
+
+    public void DisableAttack()
+    {
+        canAttack = false;
+    }
+
+    public void EnableAttack()
+    {
+        canAttack = true;
+    }
+
     private void FixedUpdate()
     {
+        //Process the player movement for this frame...
         if (canMove)
         {
             Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
+            //Face the player in the right direction...
             if ((direction.x > 0 && !facingRight)|| (direction.x < 0 && facingRight))
             {
                 transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
                 facingRight = !facingRight;
             }
 
+            //Play the walking animation (or don't)...
+            if (direction.magnitude > 0)
+            {
+                anim.SetBool(walkingAnimatorKey, true);
+            }
+            else
+            {
+                anim.SetBool(walkingAnimatorKey, false);
+            }
+
+            //Actually move the player.
             Move(direction);
         }
+        else //When not moving don't play the moving animation.
+        {
+            anim.SetBool(walkingAnimatorKey, false);
+        }
+
+        //When player can attack and wants to, attack.
+        if (Input.GetMouseButton(0) && canAttack)
+        {
+            anim.SetTrigger(lightAttackAnimatorKey);
+        }
+
     }
 }
